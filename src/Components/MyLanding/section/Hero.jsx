1bc5ee86c13cbb08b7f1017a1be204ec1d6e390a
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-01-18 01:33:15 +0600

    hero

--- src/Components/MyLanding/section/Hero.jsx
+++ src/Components/MyLanding/section/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="flex flex-col lg:h-screen lg:reltive">
 
         <div className=' text-center lg:absolute pt-20 lg:pt-52 z-50'>
-            <h1 className='lg:w-3/4 mx-auto text-primary text-2xl lg:text-4xl font-bold'></h1>
+            <h1 className='lg:w-3/4 mx-auto text-primary text-2xl lg:text-4xl font-bold'>আপনার ব্যবসার জন্য আধুনিক পোর্টফোলিও এবং রিঅ্যাক্ট ল্যান্ডিং পেজ তৈরি করুন!</h1>
             <img src={hero_image} alt="T" className='h-full pt-20 lg:hidden'/>
             <p className='text-white text-xl lg:2xl mt-5'>পেশাদার ডিজাইন এবং ফাস্ট লোডিং ল্যান্ডিং পেজ, যা আপনার বিক্রি বাড়াতে সাহায্য করবে।</p>
 
